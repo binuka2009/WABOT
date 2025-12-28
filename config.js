98fafc49c771ff5b3cd8f385558c1492bdb81113
Binuka Lasad Udayanga <binukaudayanga3@gmail.com>
2025-12-29 02:00:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "l2xRQQqA#3511n6uziXUSNwZJBKG_00PqsUDEqpv-IQDLGFm_sgA",
 ALIVE_MSG: process.env.ALIVE_MSG || "Your Alive Msg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/30vkzb.png",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
