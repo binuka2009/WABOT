ae952963fa49cdf64d92ff5a7f0c623a62ddb19d
Binuka Lasad Udayanga <binukaudayanga3@gmail.com>
2025-12-29 00:05:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "Your Session ID",
+ALIVE_MSG: process.env.ALIVE_MSG || "Your Alive Msg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/30vkzb.png",
 };
