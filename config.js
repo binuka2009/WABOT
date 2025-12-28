f799cc552f32e2afe2abb60a8f2c4206c6d9c25c
Binuka Lasad Udayanga <binukaudayanga3@gmail.com>
2025-12-29 02:41:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID || "l2xRQQqA#3511n6uziXUSNwZJBKG_00PqsUDEqpv-
 ALIVE_MSG: process.env.ALIVE_MSG || "Your Alive Msg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/30vkzb.png",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "private",
 };
