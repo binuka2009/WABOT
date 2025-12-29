d0690dfd9b5bfef4ae3580e4629f9ac369cd4e0c
Binuka Lasad Udayanga <binukaudayanga3@gmail.com>
2025-12-29 12:46:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "l2xRQQqA#3511n6uziXUSNwZJBKG_00PqsUDEqpv-IQDLGFm_sgA",
-ALIVE_MSG: process.env.ALIVE_MSG || "Your Alive Msg",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/30vkzb.png",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+MONGODB: process.env.MONGODB || "mongodb url",
 };
